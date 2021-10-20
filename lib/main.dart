d7526a448ac3bc5539f92674992b7d2f5a041aea
Nuffypuff <nlang4d@gmailcom>
2021-10-20 16:05:45 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() {}
+
+class myApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello'));
+  }
+}
